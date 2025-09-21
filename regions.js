5347c40db0f4e1c097c3e328605f7922183593ad
palerider1975-games <palerider1975@gmail.com>
2025-09-21 13:59:09 -0400

    Create regions.js

---
+++ regions.js
@@ -0,0 +1,5 @@
+export const REGION_NAMES_BY_LEVEL = {
+    1: ["Frostpine Vale","Elderglen","Blackrock Wastes","Sunspire Coast","Mistfen Marsh","Ivory Highlands","Stormreach Flats","Cinderwood","Starfall Plains","Grimwater"],
+    2: ["Whispering Peaks", "Gloomfang Forest", "The Ashen Desert", "Serpent's Coil River", "The Sunken City", "Dragon's Tooth Mountains", "The Howling Steppes", "Crimson Mire", "The Shattered Isles", "Moonshadow Glade"],
+    3: ["Celestial Spire", "Valley of Echoing Souls", "The Obsidian Plains", "Citadel of the Damned", "The Emerald Labyrinth", "Sea of Lost Stars", "The Blighted Paradise", "Giants' Graveyard", "The Chimeric Preserve", "Aethelgard's Last Stand"]
+};
