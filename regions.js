9bb4dd6609ffe445a7162bffee61468ab0522e9b
palerider1975-games <palerider1975@gmail.com>
2025-09-21 14:06:44 -0400

    Update regions.js

--- regions.js
+++ regions.js
@@ -1,5 +1,8 @@
 export const REGION_NAMES_BY_LEVEL = {
     1: ["Frostpine Vale","Elderglen","Blackrock Wastes","Sunspire Coast","Mistfen Marsh","Ivory Highlands","Stormreach Flats","Cinderwood","Starfall Plains","Grimwater"],
     2: ["Whispering Peaks", "Gloomfang Forest", "The Ashen Desert", "Serpent's Coil River", "The Sunken City", "Dragon's Tooth Mountains", "The Howling Steppes", "Crimson Mire", "The Shattered Isles", "Moonshadow Glade"],
-    3: ["Celestial Spire", "Valley of Echoing Souls", "The Obsidian Plains", "Citadel of the Damned", "The Emerald Labyrinth", "Sea of Lost Stars", "The Blighted Paradise", "Giants' Graveyard", "The Chimeric Preserve", "Aethelgard's Last Stand"]
+    3: ["Celestial Spire", "Valley of Echoing Souls", "The Obsidian Plains", "Citadel of the Damned", "The Emerald Labyrinth", "Sea of Lost Stars", "The Blighted Paradise", "Giants' Graveyard", "The Chimeric Preserve", "Aethelgard's Last Stand"],
+    4: ["Azure Archipelago", "The Sunken Kingdom", "Crystalline Forest", "Forge of the Fire Giants", "Sky-Titan's Perch", "The Maelstrom's Eye", "Whispering Labyrinth", "The Shifting Sands", "Grave of the First King", "The Verdant Maw"],
+    5: ["The Void's Edge", "Nexus of Realms", "The Final Bastion", "Where Stars Fall", "The God-King's Throne", "The Genesis Wellspring", "The Unraveling Spire", "Everlasting Glacier", "The Heart of the World", "The Sundered Throne"]
 };
+
