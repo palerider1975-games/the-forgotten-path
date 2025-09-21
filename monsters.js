0eed49fb3ccfa309165275532b876c9e1ce70816
palerider1975-games <palerider1975@gmail.com>
2025-09-21 13:53:28 -0400

    Create monsters.js

---
+++ monsters.js
@@ -0,0 +1,6 @@
+export const DUNGEON_MONSTERS = [
+    { name: "Giant Rat", emoji: "🐀" },
+    { name: "Goblin Scout", emoji: "👺" },
+    { name: "Skeleton Warrior", emoji: "💀" },
+    { name: "Cave Spider", emoji: "🕷️" },
+];
